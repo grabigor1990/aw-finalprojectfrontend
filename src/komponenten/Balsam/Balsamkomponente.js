cf4083c63aaf36ab8cd8ee4d888c6e4d2e79730f
keet <keet@huelsdau.com>
2023-12-26 23:03:04 +0100

    Anfänge beim Balsamkomponent

--- src/komponenten/Balsam/Balsamkomponente.js
+++ src/komponenten/Balsam/Balsamkomponente.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, {useId, useState} from 'react';
 
-function Balsamkomponente(props) {
+function Balsamkomponente() {
+    const [balsamListe, setBalsamListe] = useState([]);
+    const [aktuellerBalsam, setAktuellerBalsam] = useState('');
+
+    const eingebenDesBalsams = (e) => {
+        setAktuellerBalsam(e.target.value);
+    };
+
+    const bestaetigenDerEingabe = (e) => {
+        if(e.key === 'Enter' && aktuellerBalsam.trim() !== ''){
+            setBalsamListe([...balsamListe, {name: aktuellerBalsam, checked: true}]);
+            setAktuellerBalsam('');
+        }
+    }
+
+    const umschaltenDerCheckbox = (index) => {
+        const aktualisierteBalsamListe = [...balsamListe];
+        aktualisierteBalsamListe[index].checked = !aktualisierteBalsamListe[index].checked;
+        setBalsamListe(aktualisierteBalsamListe);
+    }
+
+    const loescheBalsam = (index) => {
+        const aktualisierteBalsamListe = [...balsamListe];
+        aktualisierteBalsamListe.splice(index,1);
+        setBalsamListe(aktualisierteBalsamListe);
+    }
+
     return (
         <div className="Balsamkomponente">
-            <div>Balsamkomponente
-                <p>Ideen, was in die Balsamkomponente gehört:</p>
+            <div>
+                <h3>Balsamkomponente</h3>
+                <input type="text"
+                value={aktuellerBalsam}
+                onChange={eingebenDesBalsams}
+                onKeyPress={bestaetigenDerEingabe}
+                placeholder="Balsam eingeben und Enter drücken"
+                />
                 <ul>
-                    <li>Balsam</li>
+                    {balsamListe.map((balsam, index) => (
+                        <li key={index}>
+                            {balsam.name}
+                            <input type="checkbox"
+                            checked={balsam.checked}
+                            onChange={() => umschaltenDerCheckbox(index)}
+                            />
+                            <button onClick ={() => loescheBalsam(index)}>🗑</button>
+                        </li>
+                    ))}
                 </ul>
-
             </div>
-
         </div>
     );
 }
