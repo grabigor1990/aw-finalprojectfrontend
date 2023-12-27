03cb6e0813b8159a4522e641ef5d27b09e1d186d
keet <keet@huelsdau.com>
2023-12-27 14:40:29 +0100

    Temporärer Login, um beim Frontend sich einloggen zu können, um die Komponente testen zu können, da es ansonsten CORS Konflikte gibt.
     Balsamkomponente angefangen auf axios umzuschreiben, was jedoch noch fehlerhaft ist.

--- src/komponenten/Balsam/Balsamkomponente.js
+++ src/komponenten/Balsam/Balsamkomponente.js
@@ -1,20 +1,37 @@
-import React, {useId, useState} from 'react';
+import
+    React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function Balsamkomponente() {
     const [balsamListe, setBalsamListe] = useState([]);
     const [aktuellerBalsam, setAktuellerBalsam] = useState('');
 
-    const eingebenDesBalsams = (e) => {
-        setAktuellerBalsam(e.target.value);
+
+    //Funktion zum Abrufen der Balsamliste beim Laden der Komponente
+    const fetchBalsamListe = async () => {
+        try {
+            const antwort = await axios.get('http://localhost:8080/balsame');
+            setBalsamListe(antwort.data);
+        } catch (error) {
+            console.error('Fehler beim Abrufen der Balsamliste: ', error);
+        }
     };
 
-    const bestaetigenDerEingabe = (e) => {
-        if(e.key === 'Enter' && aktuellerBalsam.trim() !== ''){
-            setBalsamListe([...balsamListe, {name: aktuellerBalsam, checked: true}]);
-            setAktuellerBalsam('');
-        }
-    }
+    useEffect(() => {
+        fetchBalsamListe();
+    }, []);
 
+    const bearbeiteBalsamHinzufuegen = async () => {
+        if (aktuellerBalsam.trim() !== '') {
+            try {
+                await axios.post('http://localhost:8080/balsam', {bezeichnung: aktuellerBalsam});
+                setAktuellerBalsam('');
+                await fetchBalsamListe();
+            } catch (error) {
+                console.error('Fehler beim Hinzufügen des Balsams: ', error);
+            }
+        }
+    };
     const umschaltenDerCheckbox = (index) => {
         const aktualisierteBalsamListe = [...balsamListe];
         aktualisierteBalsamListe[index].checked = !aktualisierteBalsamListe[index].checked;
@@ -23,7 +40,7 @@ function Balsamkomponente() {
 
     const loescheBalsam = (index) => {
         const aktualisierteBalsamListe = [...balsamListe];
-        aktualisierteBalsamListe.splice(index,1);
+        aktualisierteBalsamListe.splice(index, 1);
         setBalsamListe(aktualisierteBalsamListe);
     }
 
@@ -32,20 +49,20 @@ function Balsamkomponente() {
             <div>
                 <h3>Balsamkomponente</h3>
                 <input type="text"
-                value={aktuellerBalsam}
-                onChange={eingebenDesBalsams}
-                onKeyPress={bestaetigenDerEingabe}
-                placeholder="Balsam eingeben und Enter drücken"
+                       value={aktuellerBalsam}
+                       onChange={(e) => setAktuellerBalsam(e.target.value)}
+                       placeholder="Balsam eingeben und Enter drücken"
                 />
+                <button onClick={bearbeiteBalsamHinzufuegen}>Hinzufügen</button>
                 <ul>
                     {balsamListe.map((balsam, index) => (
                         <li key={index}>
                             {balsam.name}
                             <input type="checkbox"
-                            checked={balsam.checked}
-                            onChange={() => umschaltenDerCheckbox(index)}
+                                   checked={balsam.checked}
+                                   onChange={() => umschaltenDerCheckbox(index)}
                             />
-                            <button onClick ={() => loescheBalsam(index)}>🗑</button>
+                            <button onClick={() => loescheBalsam(index)}>🗑</button>
                         </li>
                     ))}
                 </ul>
@@ -53,5 +70,4 @@ function Balsamkomponente() {
         </div>
     );
 }
-
 export default Balsamkomponente;
