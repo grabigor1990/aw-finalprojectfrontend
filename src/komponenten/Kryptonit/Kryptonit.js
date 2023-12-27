c46b9825a23eba9813886b6afcdec1c81822c2ac
Moritz Rose <moritz_rose@yahoo.de>
2023-12-27 15:21:24 +0100

    Layout neu

--- src/komponenten/Kryptonit/Kryptonit.js
+++ src/komponenten/Kryptonit/Kryptonit.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './Kryptonit.css';
+import axios from "axios";
+
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers['Content-Type'] = 'application/json';
+
+
+
+/*Das ist ein einzelnes Kryptonit!*/
 
 function Kryptonit(props) {
-    return (
-        <div className="Kryptonit">
 
+    function handleChange(event) {
+        //console.log(haeufigkeit)
+        axios({
+            method: "post",
+            url: "http://localhost:8080/kryptonitEintrag/"+props.kryptonitId,
+            data: {
+                haeufigkeit: event.target.value
+            },
+        })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    return (
+        <div className="kryptonit">
+            <p>{props.bezeichnung}</p>
+            <div className="slider">
+                <input onChange={handleChange} type="range" min="0" max="6" step="1"/>
+                <span></span>
+            </div>
         </div>
     );
 }
--- src/komponenten/Kryptonit/Kryptonitkomponente.js
+++ src/komponenten/Kryptonit/Kryptonitkomponente.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Kryptonit from "./Kryptonit";
+import axios from "axios"
 
+/*Das ist die KryptonitListe!*/
 function Kryptonitkomponente(props) {
+
+    axios.defaults.withCredentials = true;
+    axios.defaults.headers['Content-Type'] = 'application/json';
+
+    const [kryptonite, setKryptonite] = useState([]);
+
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/kryptonite")
+            .then(response => {
+                setKryptonite(response.data.daten)
+            })
+            .catch(error => {
+                console.error(error.response.data[0]);
+            });
+    },[]);
+
     return (
         <div className="Kryptonitkomponente Komponente">
-            Kryptonitkomponente
+            <div>
+            {kryptonite ? kryptonite.map((kryptonit, index) => (
+                <Kryptonit key={index} kryptonitId={kryptonit.kryptonitId} bezeichnung={kryptonit.bezeichnung} />
+            )) : <p>Trage hier die Angewohnheiten ein, die du tracken möchtest.</p>}
+            </div>
         </div>
     );
 }
