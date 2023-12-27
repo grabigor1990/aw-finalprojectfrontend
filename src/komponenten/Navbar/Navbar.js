c19085a8c629b892f753d381860f4d4176df1783
[Grab I.] <grabigor1990@gmail.com>
2023-12-27 18:08:28 +0100

    ausloggen funktioniert

--- src/komponenten/Navbar/Navbar.js
+++ src/komponenten/Navbar/Navbar.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import "./navbar.css"; // Stellen Sie sicher, dass Ihre CSS-Datei korrekt importiert ist.
 
-function Navbar(props) {
+const Navbar = () => {
+    const handleLogout = async () => {
+        try {
+            await axios.post('http://localhost:8080/ausloggen');
+            console.log("ausgeloggt");
+        } catch (error) {
+            console.error('Fehler beim Ausloggen:', error);
+        }
+    };
+
     return (
-        <div className="Navbar"></div>
+        <div className="navbar">
+
+            <button onClick={() => window.location.href = '/profil-bearbeiten'}>Profil bearbeiten</button>
+            <img src="https://cdn.icon-icons.com/icons2/1369/PNG/512/-mood_90670.png" alt="Logo" />
+
+
+            <button onClick={handleLogout}>Ausloggen</button>
+        </div>
     );
-}
+};
 
 export default Navbar;
